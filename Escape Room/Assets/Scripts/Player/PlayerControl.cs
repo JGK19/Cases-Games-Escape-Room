24de1e88881ed4fef65d83f5fae69b1ef396e65c
JGK19 <jgkongevold@outlook.com>
2023-05-25 09:24:19 -0300

    Adicionando colisão ao player
    Mudei o código pra fazer o player se movimentar por vetores e usar a colisão do rigidbody, criei tbm um prefab pra botar nas paredes onde o player não pode passar
    
    Co-Authored-By: renatoAlves11 <132839322+renatoAlves11@users.noreply.github.com>

--- Escape Room/Assets/Scripts/Player/PlayerControl.cs
+++ Escape Room/Assets/Scripts/Player/PlayerControl.cs
@@ -4,42 +4,52 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public Rigidbody2D body;
     public float moveSpeed;
     private bool isMoving;
     private Vector2 input;
     private Animator animator;
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
+
     private void Update()
     {
-        if (!isMoving)
-        {
+        
+        
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
             if (input.x != 0) input.y = 0;
-            if(input != Vector2.zero)
-            {
-                animator.SetFloat("MoveX", input.x);
-                animator.SetFloat("MoveY", input.y);
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
-                StartCoroutine(Move(targetPos));
-            }
-        }
+            
+            
+            animator.SetFloat("MoveX", input.x);
+            animator.SetFloat("MoveY", input.y);
+            
+
+            /*var targetPos = transform.position;
+            targetPos.x += input.x;
+            targetPos.y += input.y;*/
+
+            body.velocity = input.normalized * moveSpeed; //* Time.fixedDeltaTime; não funciona por alguma razão quando multiplica pelo deltatime
+
+                //StartCoroutine(Move(targetPos));
+            
+        if (input.x != 0 || input.y != 0) isMoving = true;
+        if (input.x == 0 && input.y == 0) isMoving = false;
+
         animator.SetBool("isMoving", isMoving);
+
     }
-    IEnumerator Move(Vector3 targetPos)
+
+    /*IEnumerator Move(Vector3 targetPos)
     {
         isMoving = true;
         while((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
         {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
             yield return null;
         }
-        transform.position = targetPos;
         isMoving = false;
-    }
+    }*/
 }
