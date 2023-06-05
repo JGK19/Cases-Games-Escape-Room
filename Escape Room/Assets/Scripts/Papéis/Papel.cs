4daa4ff391f1e03db2f90fc8d9e6eb52d5c2a83e
renatoAlves11 <renato.alvs1103@gmail.com>
2023-06-05 11:21:55 -0300

    papéis

---
+++ Escape Room/Assets/Scripts/Papéis/Papel.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Papel : MonoBehaviour
+{    
+    public SpriteRenderer papel1_1;
+    private bool triggerEntered;
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.E)&& triggerEntered == true)
+        {
+        papel1_1.GetComponent<SpriteRenderer>().enabled=true;
+        }
+        if (triggerEntered == false)
+        {
+          papel1_1.GetComponent<SpriteRenderer>().enabled=false;   
+        }
+    }
+    private void OnTriggerEnter2D (Collider2D collision)
+    {  
+           triggerEntered = true;
+        
+    }
+    private void OnTriggerExit2D (Collider2D collision)
+    {
+           triggerEntered = false;
+        
+    }
+}
